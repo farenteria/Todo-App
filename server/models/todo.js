ee290ef1fdaa3b659cbb40f3d1a8236a71f815cc
Fernando Renteria <rentfern@gmail.com>
2018-07-06 12:02:20 -0500

    Refactored code. Added mocha testing suite and test cases

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+// trim removes extra white space from beginning/end of string
+var Todo = mongoose.model("Todo", {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = {Todo};
--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
+var express = require("express");
+var bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+var {mongoose} = require ("./db/mongoose.js");
+var {Todo} = require ("./models/todo");
+var {User} = require("./models/user");
 
-var Todo = mongoose.model("Todo", {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) =>{
+        res.status(400).send(e);
+    });
 });
 
-var newTodo = new Todo({
-    text: "Cook dinner",
-    completed: false,
-    completedAt: 1234
+app.listen(3000, () => {
+    console.log("Started on port 3000");
 });
 
-newTodo.save().then((doc) => {
-    console.log("saved todo", doc);
-}, (err) => {
-    console.log("Unable to save todo");
-});
+module.exports = {app};
