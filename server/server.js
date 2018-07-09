9b362d20960933d8d39c9661bc6ae90e422b245d
Fernando Renteria <rentfern@gmail.com>
2018-07-09 15:46:07 -0500

    Added additional test cased for specific todo items

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require ("./db/mongoose.js");
 var {Todo} = require ("./models/todo");
 var {User} = require("./models/user");
 
+const {ObjectID} = require("mongodb");
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -14,6 +16,24 @@ app.get("/todos", (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get("/todos:id", (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
